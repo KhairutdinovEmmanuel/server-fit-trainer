bc30fc4ea9a31e7f80c1da32bdbc8527eeb2babf
Emmanuel <hairutdinov.emmanuwl@gmail.com>
2022-05-12 13:40:59 +0300

    Add route forgot password with return jwt token logic

--- validations/auth.ts
+++ validations/auth.ts
@@ -3,6 +3,7 @@ import {combineValidation} from "../utils/validationHelper";
 import {
 	RequestAuth, ResponseAuth,
 	RequestVerify, ResponseVerify,
+	RequestForgot, ResponseForgot
 } from "../types/ControllerAuth";
 import errorMessages from "./errorMessages";
 import customValidations from "./customValidations";
@@ -26,6 +27,24 @@ export const validationVerify = combineValidation<RequestVerify, ResponseVerify>
 		.withMessage(errorMessages.MESSAGE_ERROR_TYPE_STRING)
 		.isEmail()
 		.withMessage(errorMessages.MESSAGE_ERROR_EMAIL),
+	body("confirmCode")
+		.custom(customValidations.isNumber)
+		.withMessage(errorMessages.MESSAGE_ERROR_TYPE_NUMBER)
+		.isLength({ min: 6, max: 6 })
+		.withMessage(errorMessages.MESSAGE_ERROR_CONFIRM_CODE),
+])
+
+export const validationForgot = combineValidation<RequestForgot, ResponseForgot>([
+	body("email")
+		.isString()
+		.withMessage(errorMessages.MESSAGE_ERROR_TYPE_STRING)
+		.isEmail()
+		.withMessage(errorMessages.MESSAGE_ERROR_EMAIL),
+	body("password")
+		.isString()
+		.withMessage(errorMessages.MESSAGE_ERROR_TYPE_STRING)
+		.isLength({ min: 6, max: 12 })
+		.withMessage(errorMessages.MESSAGE_ERROR_PASSWORD),
 	body("confirmCode")
 		.custom(customValidations.isNumber)
 		.withMessage(errorMessages.MESSAGE_ERROR_TYPE_NUMBER)
